36ccdb1aa0ccdf5b8fed5aad94103dee3a161851
kathd <kathleensdomingo@gmail.com>
2017-10-18 21:30:50 -0700

    Fixed: trip (add, edit) + board (add, edit) + item (add)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
 
+  def show
+    @trip = current_user.trips.find(params[:trip_id])
+    @board = @trip.boards.find(params[:board_id])
+    @item = @board.items.find(params[:item_id])
+  end
+
   def new
     @item = Item.new
   end
@@ -21,7 +27,20 @@ class ItemsController < ApplicationController
   end
 
   def update
-    
+    trip = current_user.trips.find(params[:trip_id])
+    board = trip.boards.find(params[:board_id])
+    item = board.items.find(params[:item_id])
+    item.update(
+      board_id: @board.id,
+      item_type: params[:item_type],
+      notes: params[:notes],
+      time: params[:time],
+      longitude: params[:longitude],
+      latitude: params[:latitude],
+      image_url: params[:image_url],
+      price: params[:price]
+      )
+    redirect_to "/trips/#{@trip.id}"
   end
 
   def destroy
